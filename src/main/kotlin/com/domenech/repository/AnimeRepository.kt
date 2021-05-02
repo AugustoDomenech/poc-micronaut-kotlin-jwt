1c9bf776cadb645d8c16aa6a68107a6eef8869be
Guz <augustodomenech@gmail.com>
2021-05-01 23:10:22 -0300

    Added repository class Anime

---
+++ src/main/kotlin/com/domenech/repository/AnimeRepository.kt
@@ -0,0 +1,8 @@
+package com.domenech.repository
+
+import com.domenech.domain.entity.Anime
+import io.micronaut.data.annotation.Repository
+import io.micronaut.data.jpa.repository.JpaRepository
+
+@Repository
+interface AnimeRepository: JpaRepository<Anime, Long> {}
